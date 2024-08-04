d4be77935e9b4f5ee2a74de2ff7ce09537328526
Kerolaine-Nunes <kerolaine@magusdev.com.br>
2024-08-04 18:43:32 -0300

    Options finalizadas até escritório

--- utils/listContainersForImages.js
+++ utils/listContainersForImages.js
@@ -48,9 +48,67 @@ export const escadaOptions = [
     { value: 'suspensa', label: 'Escada suspensa', img: '/images/escadas/4.jpg' },
   ];
 
-  export const levantamentoAreaGaragemOptions = [
-    { value: '1', label: '1 Pavimento', img: '/images/Pavimentos/1Pavimento.png'},
-    { value: '2', label: '2 Pavimentos', img: '/images/Pavimentos/2Pavimentos.png'},
-    { value: '3', label: '3 Pavimentos', img: '/images/Pavimentos/3Pavimentos.png'},
+export const levantamentoAreaGaragemOptions = [
+    { value: 'areaGaragemCoberta', label: 'Área total de Garagem Coberta', img: '/images/GaragemTelheiros/levantamentoAreas/image389.png'},
+    { value: 'areaTelhereiros', label: 'Área total de telheiros', img: '/images/GaragemTelheiros/levantamentoAreas/image392.png'},
   ];
   
+export const fundaçãoOptions = [
+    { value: 'radier', label: 'Radier', img: '/images/GaragemTelheiros/Fundação/image403.png'},
+    { value: 'lajePiso', label: 'Laje Piso', img: '/images/GaragemTelheiros/Fundação/image405.png'},
+    { value: 'lajePisoImpermeabilizada', label: 'Laje Piso', img: '/images/GaragemTelheiros/Fundação/image407.png'},
+  ];
+
+export const pisosPorcelanatoOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPisoPorcelanato/image681.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPisoPorcelanato/image683.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPisoPorcelanato/image685.png'},
+  ]; 
+
+export const pisosLaminadoOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPisoLaminado/image695.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPisoLaminado/image697.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPisoLaminado/image699.png'},
+  ];
+
+export const peitorilSoleirasOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPeitorilSoleiras/image711.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPeitorilSoleiras/image713.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPeitorilSoleiras/image715.png'},
+  ];
+
+export const portasMadeiraOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPortasMadeira/image720.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPortasMadeira/image723.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPortasMadeira/image724.png'},
+    { value: 'outras', label: 'Outros Tipos de Portas', img: null}
+  ]; 
+
+export const portasJanelasEsquadriasOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPortasJanelasEsquadrias/image733.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPortasJanelasEsquadrias/image736.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPortasJanelasEsquadrias/image739.png'},
+  ];
+
+export const pinturaInternaOptions = [
+    { value: 'standard', label: 'Standard', img: '/images/GaragemTelheiros/acabamentosPinturaInterna/image745.png'},
+    { value: 'premium', label: 'Premium', img: '/images/GaragemTelheiros/acabamentosPinturaInterna/image745Copia.png'},
+    { value: 'supreme', label: 'Supreme', img: '/images/GaragemTelheiros/acabamentosPinturaInterna/image745Copia(2).png'},
+    { value: 'forro', label: 'Forro', img: '/images/GaragemTelheiros/acabamentosPinturaInterna/image744.png'},
+  ];
+
+export const levantamentoAreaSalaOptions = [
+    { value: 'salaEstar', label: 'Sala de Estar/Visitas', img: '/images/SalaHallCorredores/levantamentoAreas/image497.png'},
+    { value: 'salaTV', label: 'Sala de TV', img: '/images/SalaHallCorredores/levantamentoAreas/image499.png'},
+    { value: 'corredores', label: 'Corredores', img: '/images/SalaHallCorredores/levantamentoAreas/image501.png'},
+  ];
+
+export const levantamentoAreaEscritórioOptions = [
+    { value: 'escritório', label: 'Escritório', img: '/images/EscritórioDespensa/levantamentoAreas/image590.png'},
+    { value: 'despensaV', label: 'Despensa', img: '/images/EscritórioDespensa/levantamentoAreas/image592.png'},
+];
+
+export const levantamentoAreaQuartoOptions = [
+  { value: 'escritório', label: 'Escritório', img: '/images/EscritórioDespensa/levantamentoAreas/image590.png'},
+  { value: 'despensaV', label: 'Despensa', img: '/images/EscritórioDespensa/levantamentoAreas/image592.png'},
+];
